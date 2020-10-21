ef3f918b2210b897371cfef469d4a8c53da4922f
Doğukan Kürekci <49594791+dogukankurekci@users.noreply.github.com>
2020-10-21 18:21:25 +0300

    text based game added

---
+++ Assets/Scripts/Manager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Manager : MonoBehaviour
+{
+    public Database database;
+    public Question currentQuestion;
+
+    public Text questionText;
+    public Text optionAText;
+    public Text optionBText;
+
+    public int randomIndex;
+
+    public GameObject endGame;
+
+    public void Awake()
+    {
+        getQuestion();
+    }
+
+    public void Update()
+    {
+        if(database.questions.Count == 0)
+        {
+            endGame.SetActive(true);
+        }
+    }
+
+    void getQuestion()
+    {
+        randomIndex = Random.Range(0, database.questions.Count - 1);
+        currentQuestion = database.questions[randomIndex];
+
+        questionText.text = currentQuestion.questionText;
+        optionAText.text = currentQuestion.optionA;
+        optionBText.text = currentQuestion.optionB;
+    }
+
+    public void toAnswer(string option)
+    {
+        database.questions.RemoveAt(randomIndex);
+
+        if(option == currentQuestion.Answer)
+        {
+            Debug.Log("Tebrikler doğru cevapladınız.");
+            getQuestion();
+        }
+
+        else
+        {
+            Debug.Log("Maalesef yanlış cevapladınız.");
+            getQuestion();
+        }
+    }
+}
---
+++ Assets/Scripts/Question.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+
+[System.Serializable]
+public class Question
+{
+    public string questionText;
+    public string optionA;
+    public string optionB;
+    public string Answer;
+
+    void sorular(string qText, string o_A, string o_B, string answer)
+    {
+        qText = questionText;
+        o_A = optionA;
+        o_B = optionB;
+        answer = Answer;
+    }
+
+}
